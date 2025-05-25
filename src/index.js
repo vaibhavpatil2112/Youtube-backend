9f90441455fed992879bf5ebb1d02f56767ab823
vaibhav patil~ <vaibhavpatil211299@gmail.com$ git config --global user.name vaibhav>
2025-05-25 23:04:32 +0530

    DB connection to mongo Atlas

---
+++ src/index.js
@@ -0,0 +1,40 @@
+// require('dotenv').config({path: './env'})         Old method don't use
+
+import dotenv from "dotenv";
+import connectDB from "./db/database.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+
+// function connectDB(){}    old approch
+// connectDB()
+
+/*
+const app = express()(
+
+
+  async () => {
+    try {
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+      app.on("error", (error) => {
+        console.log("ERROR", error);                                          
+        throw error;
+      });
+                                                                                      //    good approach
+
+      app.listen(process.env.PORT, () => {
+        console.log(`app is listening on port ${process.env.PORT}`);
+      });
+    } catch (error) {
+      console.error("ERROR", error);
+      throw err;
+    }
+  }
+)
+// ();
+*/
